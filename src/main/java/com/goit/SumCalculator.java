0f5f94939bdcc0a6b83e48b207a244b07fd18e1f
A1MaHaiD <super.lukianchuk2010@gmail.com>
2023-04-14 17:22:39 +0300

    replaced sum()

--- src/main/java/com/goit/SumCalculator.java
+++ src/main/java/com/goit/SumCalculator.java
@@ -1,6 +1,7 @@
 package com.goit;
 
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class SumCalculator {
     public static void main(String[] args) {
@@ -9,14 +10,10 @@ public class SumCalculator {
         System.out.println(sum(number));
     }
 
-        public static int sum(int n) {
-        if (n < 0) {
+    public static int sum(int n) {
+        if (n <= 0) {
             throw new IllegalArgumentException("" + n + " must be more than 0");
         }
-        int result = 0;
-        for (int i = 1; i <= n; i++) {
-            result += i;
-        }
-        return result;
+        return IntStream.rangeClosed(0, n).sum();
     }
 }
